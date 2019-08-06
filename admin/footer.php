faa8a3bafccee00800e23121d98ef15baefbd0a7
NitishKumarGarg <53821812+NitishKumarGarg@users.noreply.github.com>
2019-08-07 02:02:31 +0530

    Add files via upload

---
+++ admin/footer.php
@@ -0,0 +1,14 @@
+<div class="col-md-12">
+<footer class="footer">
+<div style="color:yellow;">
+<strong>RAILWAY RESERVATION MANAGEMENT SYSTEM<br>Ayush Kumar<br>Nitish Kumar Garg<br>
+</div>
+<div style="color:yellow;">
+<strong>RVCE</strong>
+</div>
+</footer>
+</div>
+</div>
+</div>
+</body>
+</html>
